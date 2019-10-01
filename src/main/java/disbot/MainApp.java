fee1b3bed8356dedff82b414e6f4113ffc6925cb
Arus03 <kimjng10@naver.com>
2019-10-01 15:16:22 +0900

    런치파서까지 제작완료

---
+++ src/main/java/disbot/MainApp.java
@@ -0,0 +1,24 @@
+package disbot;
+
+import listener.LunchListener;
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		JDABuilder builder = new JDABuilder(AccountType.BOT);
+		String token = "";
+		
+		builder.setToken(token);
+		
+		try {
+			builder.addEventListeners(new LunchListener());
+			builder.build();
+		} catch (Exception e) {
+		e.printStackTrace();
+		}
+
+	}
+
+}
